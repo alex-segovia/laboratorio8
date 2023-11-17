ae52caeb50d9499a80bc56aa38b48bc4039871e9
Gabriel <gabriel_alejandro.tm@hotmail.com>
2023-11-17 03:14:41 -0500

    Recursos 50%, faltan metodos y comprobar las querys :,(

--- src/main/java/com/example/laboratorio8/Controllers/RecursosServlet.java
+++ src/main/java/com/example/laboratorio8/Controllers/RecursosServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 @WebServlet(name = "RecursosServlet", value = "/RecursosServlet")
@@ -33,13 +34,13 @@ public class RecursosServlet extends HttpServlet {
         // Datos
         ArrayList<Habitante> habitantesMoralBaja= daoHabitante.getHabitantesMoralBaja(jugador.getIdJugador());
         ArrayList<Float> alimentoProduccionVsConsumo = daoHabitante.getAlimentoProduccionVsConsumo(jugador.getIdJugador());
-        ArrayList<Habitante> habitantesMuertos = daoHabitante.getHabitantesMuertos(jugador.getIdJugador());
+        ArrayList<Habitante> habitantesMuertos = daoHabitante.getHabitantesMuertos(jugador.getIdJugador()); // revisarrr
 
         switch (action){
             case "listado":
                 request.setAttribute("habitantesMoralBaja",habitantesMoralBaja);
                 request.setAttribute("alimentoProduccionVsConsumo",alimentoProduccionVsConsumo);
-                request.setAttribute("habitantesMuertos",habitantesMuertos);
+                request.setAttribute("habitantesMuertos",habitantesMuertos); // revisarrr
                 break;
             default:
                 System.out.println("uwu");
@@ -70,21 +71,72 @@ public class RecursosServlet extends HttpServlet {
 
             case "pasarHoras":
                 daoJugador.pasarHoras(jugador.getIdJugador());
-                response.sendRedirect("RecursosServlet");
                 break;
 
             case "terminarDia":
 
+                // Por ver si el codigo se ejecuta aqui o no
 
+                // Alimentar a la población: MUERTES POR HAMBRE
 
+                ArrayList<Habitante> listaHabitantes = daoHabitante.getListaHabitantes(jugador.getIdJugador());
+                Float alimentoProducido = daoHabitante.getAlimentoProduccionVsConsumo(jugador.getIdJugador()).get(1);
 
+                ArrayList<Integer> indicesHabitantes = new ArrayList<>();
 
+                for(int i = 0; i<listaHabitantes.size(); i++){
+                    indicesHabitantes.add(i);
+                }
 
+                Collections.shuffle(indicesHabitantes);
 
+                Float alimentoAConsumir = 0.0f;
+                float moralPerdida = 0.0f;
+
+                for(Integer j: indicesHabitantes){
+
+                    Habitante habitante = listaHabitantes.get(j);
+
+                    alimentoAConsumir = habitante.getAlimentacionDiaria();
+
+                    if(alimentoProducido>=0.0f){
+                        if((alimentoProducido-alimentoAConsumir)>0.0f){
+                            alimentoProducido = alimentoProducido - alimentoAConsumir;
+                        }else{
+                            moralPerdida = habitante.getMoral() - (alimentoAConsumir - alimentoProducido);
+                            if(moralPerdida <= 0.0f){
+                                daoHabitante.updateMoral(jugador.getIdJugador(), habitante.getIdHabitante(), 0.0f);
+                                daoHabitante.killHabitante(jugador.getIdJugador(), habitante.getIdHabitante(),"Hambre"); // Añadir dia muerte
+                            }
+                            daoHabitante.updateMoral(jugador.getIdJugador(), habitante.getIdHabitante(), moralPerdida);
+                        }
+                    }else{
+                        alimentoProducido = 0.0f;
+                    }
+                }
+
+                // Crecimiento de la población: MUERTES POR DESESPERACIÓN
+
+                int cantidadPoblacion = listaHabitantes.size();
+                int diasPasados = jugador.getDiasDesdeCreacion();
+
+                if(cantidadPoblacion>4*diasPasados){
+                    System.out.println("Aiuda");
+                }else{
+                    // Se puede hacer de otra manera pero noseeeee
+                    daoHabitante.updateMoralMultiple(jugador.getIdJugador(), "Desesperación"); // Añadir dia muerte
+                }
+
+                // Subir moral:
+
+                daoHabitante.updateMoralMultiple(jugador.getIdJugador());
+
+                // Se termina el día:
 
                 daoJugador.terminarDia(jugador.getIdJugador());
         }
 
+        response.sendRedirect("RecursosServlet");
         httpSession.setAttribute("jugadorLogueado",daoJugador.obtenerJugadorPorId(jugador.getIdJugador()));
     }
 }
--- src/main/java/com/example/laboratorio8/Daos/DaoHabitante.java
+++ src/main/java/com/example/laboratorio8/Daos/DaoHabitante.java
@@ -140,6 +140,103 @@ public class DaoHabitante extends DaoBase{
         return habitantesMuertos;
     }
 
-    public void matarC
+    public void killHabitante(int idJugador, int idHabitante, String causaMuerte){
+
+    }
+
+    public void updateMoralMultiple (int idJugador, String causaMuerte){
+        String sql = "UPDATE habitante SET moral = moral-ceil(moral*0.5), estaMuerto = IF(moral-ceil(moral*0.5) = 0,?,estaMuerto), " +
+                "causaMuerte = IF(moral-ceil(moral*0.5) = 0,?,causaMuerte) WHERE idJugador = ?";
+
+        try (Connection conn = this.getConection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setBoolean(1,true);
+            pstmt.setString(2,causaMuerte);
+            pstmt.setInt(3,idJugador);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateMoralMultiple (int idJugador){
+        String sql = "UPDATE habitante SET moral = moral + (SELECT sum(produccionMoral) FROM habitante WHERE idJugador = ? GROUP BY idJugador) WHERE idJugador = ?;";
+
+        try (Connection conn = this.getConection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1,idJugador);
+            pstmt.setInt(2,idJugador);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<Habitante> getListaHabitantes(int idJugador){
+        String sql = "SELECT * FROM habitante WHERE idJugador = ? AND (estaExiliado = false AND estaMuerto = false);";
+        ArrayList<Habitante> listaHabitantes = new ArrayList<>();
+
+        try (Connection conn = this.getConection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, idJugador);
+
+            try(ResultSet rs = pstmt.executeQuery()){
+                while(rs.next()){
+                    String profesion = rs.getString(4);
+                    Habitante habitante = null;
+                    switch (profesion){
+                        case "Granjero":
+                            habitante = new Granjero();
+                            ((Granjero) habitante).setProduccionAlimento(rs.getFloat(13));
+                            break;
+                        case "Constructor":
+                            habitante = new Constructor();
+                            ((Constructor) habitante).setProduccionMoral(rs.getFloat(14));
+                            ((Constructor) habitante).setFuerza(rs.getFloat(12));
+                            break;
+                        case "Soldado":
+                            habitante = new Soldado();
+                            ((Soldado) habitante).setProduccionMoral(rs.getFloat(14));
+                            ((Soldado) habitante).setFuerza(rs.getFloat(12));
+                            break;
+                        default:
+                            habitante = new Habitante();
+                            System.out.println("Me olvidé polimorfismo xd");
+
+                    }
+
+                    habitante.setIdHabitante(rs.getInt(1));
+                    habitante.setJugador(new DaoJugador().obtenerJugadorPorId(rs.getInt(2)));
+                    habitante.setNombre(rs.getString(3));
+                    habitante.setGenero(rs.getString(5));
+                    habitante.setHorasDia(rs.getInt(6));
+                    habitante.setDiasVivo(rs.getInt(7));
+                    habitante.setEstaExiliado(rs.getBoolean(8));
+                    habitante.setEstaMuerto(rs.getBoolean(9));
+                    habitante.setAlimentacionDiaria(rs.getFloat(10));
+                    habitante.setMoral(rs.getFloat(11));
+
+                    listaHabitantes.add(habitante);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return listaHabitantes;
+    }
+
+    public void updateMoral (int idJugador, int idHabitante, Float moral){
+        String sql = "UPDATE habitante SET moral = ? WHERE idJugador = ? AND idHabitante = ?";
+
+        try (Connection conn = this.getConection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setFloat(1,moral);
+            pstmt.setInt(2,idJugador);
+            pstmt.setInt(3,idHabitante);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
