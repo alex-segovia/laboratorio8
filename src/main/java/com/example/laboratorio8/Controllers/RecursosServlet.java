390f7ca89ceca0442fd9973fe95546fd7a47f4bd
Gabriel <gabriel_alejandro.tm@hotmail.com>
2023-11-17 00:10:44 -0500

    Primeros Metodos para Recursozzzzz

--- src/main/java/com/example/laboratorio8/Controllers/RecursosServlet.java
+++ src/main/java/com/example/laboratorio8/Controllers/RecursosServlet.java
@@ -1,5 +1,8 @@
 package com.example.laboratorio8.Controllers;
 
+import com.example.laboratorio8.Beans.Jugador;
+import com.example.laboratorio8.Daos.DaoHabitante;
+import com.example.laboratorio8.Daos.DaoJugador;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -11,10 +14,40 @@ public class RecursosServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+
+
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession httpSession = request.getSession();
+
+        String action = request.getParameter("action") == null ? "listado" : request.getParameter("action");
+        Jugador jugador = (Jugador) httpSession.getAttribute("jugadorLogueado");
+
+        RequestDispatcher view;
+
+        DaoJugador daoJugador = new DaoJugador();
+        DaoHabitante daoHabitante = new DaoHabitante();
+
+
+
+        switch (action){
+
+            case "pasarHoras":
+
+
+
+
+
+                break;
+
+
+
+        }
 
     }
 }
--- src/main/java/com/example/laboratorio8/Daos/DaoJugador.java
+++ src/main/java/com/example/laboratorio8/Daos/DaoJugador.java
@@ -1,4 +1,79 @@
 package com.example.laboratorio8.Daos;
 
+import com.example.laboratorio8.Beans.Jugador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DaoJugador extends DaoBase{
+
+    public Jugador obtenerJugadorPorId(int idJugador){
+        String sql = "SELECT * FROM jugador WHERE idJugador = ?";
+        Jugador jugador = null;
+
+        try (Connection conn = this.getConection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, idJugador);
+
+            try(ResultSet rs = pstmt.executeQuery()){
+
+                if(rs.next()){
+                    jugador = llenarDatosJugador(jugador,rs);
+                }
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return jugador;
+
+    }
+
+    public void terminarDia(int idJugador){
+        String sql = "UPDATE jugador SET horasDelDia = 0, diasDesdeCreacion = diasDesdeCreacion + 1 WHERE idJugador = ?";
+
+        try (Connection conn = this.getConection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, idJugador);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void pasarHoras(int idJugador){
+
+        String sql = "UPDATE jugador SET horasDelDia = 24 WHERE idJugador = ?";
+
+        try (Connection conn = this.getConection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.setInt(1, idJugador);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Jugador llenarDatosJugador(Jugador jugador, ResultSet rs) throws SQLException {
+
+        jugador = new Jugador();
+        jugador.setIdJugador(rs.getInt(1));
+        jugador.setNombreJugador(rs.getString(2));
+        jugador.setEdad((rs.getInt(3)));
+        jugador.setCorreo(rs.getString(4));
+        jugador.setUsuario(rs.getString(5));
+        jugador.setContrasena(rs.getString(6));
+        jugador.setHorasDia(rs.getInt(7));
+        jugador.setDiasDesdeCreacion(rs.getInt(8));
+        jugador.setAldeaEnDesesperacion(rs.getBoolean(9));
+        jugador.setEstado(rs.getString(10));
+
+        return jugador;
+    }
+
 }
