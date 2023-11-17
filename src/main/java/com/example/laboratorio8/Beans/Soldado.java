2b8d5b242edfb7c34e27a55f997e7287f52043e3
alex-segovia <a20213849@pucp.edu.pe>
2023-11-16 23:41:28 -0500

    atributos de los beans

--- src/main/java/com/example/laboratorio8/Beans/Soldado.java
+++ src/main/java/com/example/laboratorio8/Beans/Soldado.java
@@ -1,4 +1,22 @@
 package com.example.laboratorio8.Beans;
 
 public class Soldado extends Habitante{
+    float produccionMoral;
+    float fuerza;
+
+    public float getProduccionMoral() {
+        return produccionMoral;
+    }
+
+    public void setProduccionMoral(float produccionMoral) {
+        this.produccionMoral = produccionMoral;
+    }
+
+    public float getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(float fuerza) {
+        this.fuerza = fuerza;
+    }
 }
