740dbf67f1c76156537a400063794fb18327325d
Gabriel <gabriel_alejandro.tm@hotmail.com>
2023-11-16 23:43:07 -0500

    uwu

--- src/main/java/com/example/laboratorio8/Daos/DaoBase.java
+++ src/main/java/com/example/laboratorio8/Daos/DaoBase.java
@@ -1,5 +1,23 @@
 package com.example.laboratorio8.Daos;
 
-public abstract class DaoBase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public abstract class DaoBase {
+    public Connection getConection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        String username = "root";
+        String password = "root";
+        String database = "laboratoriouwu";
+        String url = "jdbc:mysql://localhost:3306/" + database;
+
+        return DriverManager.getConnection(url, username, password);
+
+    }
 }
