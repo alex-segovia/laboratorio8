b2e0aa97e6c5df32309204c547522be31142b49d
alex-segovia <a20213849@pucp.edu.pe>
2023-11-17 17:35:20 -0500

    cambio en la db y lista de habitantes

---
+++ src/main/database/TombatScript.sql
@@ -0,0 +1,14 @@
+/* Base de Datos - Tombat */
+
+/* Jugadores */
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Alex",20,"correo1@gmail.com","Bulbasaur","olA23$",5,7,"En paz",300.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Josh",20,"correo2@gmail.com","Pikachu","olA23$",16,2,"En guerra",41.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Gabriel",20,"correo3@gmail.com","Squirtle","olA23$",4,9,"En paz",140.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Hineill",19,"correo4@gmail.com","Psyduck","olA23$",10,14,"En paz",50.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Santiago",20,"correo5@gmail.com","Nidoking","olA23$",9,5,"En paz",420.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Jampi",20,"correo6@gmail.com","Charmander","olA23$",18,4,"En paz",230.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Mayte",19,"correo7@gmail.com","Jigglypuff","olA23$",1,10,"En guerra",240.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Alvaro",29,"correo8@gmail.com","Eevee","olA23$",20,7,"En paz",67.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Stuardo",32,"correo9@gmail.com","Snorlax","olA23$",3,2,"En guerra",55.0);
+insert into jugador(nombre,edad,correo,usuario,contrasena,horasDelDia,diasDesdeCreacion,estado,alimentoTotal) values("Minaya",28,"correo10@gmail.com","Dragonite","olA23$",14,10,"En guerra",105.0);
+
